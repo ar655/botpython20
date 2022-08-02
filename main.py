b458afb82f635354b6994b3f083eb935726f0d10
ar655 <106866721+ar655@users.noreply.github.com>
2022-08-02 09:24:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from aiogram.utils import executor
+from config import dp
+
+import logging
+import  asyncio
+from handlers import  callback ,client , message,admin,fsmAdminMenu,notification
+from database.bot_db import sql_create
+
+
+
+client.register_handler_q(dp)
+fsmAdminMenu.register_handlers_fsmAdminmenu(dp)
+
+
+
+notification.register_func(dp)
+callback.register_handlers_query(dp)
+
+
+message.register_handler_some(dp)
+
+admin.register_handlers_extra(dp)
+
+
+async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
+    # sql_create()
+
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp,skip_updates=True,on_startup=on_startup)
